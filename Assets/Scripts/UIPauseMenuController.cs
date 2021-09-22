a84febab0f364bb3538e09e4ecf7d130472cc9cf
ramadhantaufiq <ramadhantaufiq098@gmail.com>
2021-09-22 17:21:54 +0700

    commit

---
+++ Assets/Scripts/UIPauseMenuController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIPauseMenuController : MonoBehaviour
+{
+    public GameObject pauseMenuPanel;
+    public GameObject gameplayPanel;
+    public Text statusInfo;
+    public Button buttonResume;
+    public Button buttonRestart;
+    public Button buttonExit;
+
+    private void Start()
+    {
+        buttonResume.onClick.AddListener(ResumeGame);
+        buttonRestart.onClick.AddListener(RestartGame);
+        buttonExit.onClick.AddListener(ExitToMenu);
+        pauseMenuPanel.gameObject.SetActive(false);
+    }
+
+    public void EndGame(bool isWin)
+    {
+        buttonResume.gameObject.SetActive(false);
+        statusInfo.text = isWin ? "YOU WIN!" : "YOU LOSE!";
+        gameplayPanel.SetActive(false);
+        pauseMenuPanel.gameObject.SetActive(true);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        pauseMenuPanel.gameObject.SetActive(true);
+        statusInfo.text = "GAME PAUSED";
+        gameplayPanel.SetActive(false);
+    }
+    
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pauseMenuPanel.gameObject.SetActive(false);
+        gameplayPanel.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void ExitToMenu()
+    {
+        Application.Quit();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# AngryBirds
+# Angry Bird
+Angry Birds clone game made as part of Agate Studi Independen Game Programming Course
+
+## How to play
+
+### Build
+You can play the game by accessing the build version on the [releases page](https://github.com/NaufalA/Angry-Bird/releases)
+
+## Stuff changed or added
